c825ac84b8983239f6f455b20dcdb8e97e1e9266
Igor.Koloski <koloski_igor@hotmail.com>
2021-05-27 16:10:37 +0200

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "articles#index"
+  root 'articles#index'
 
   resources :articles do
     resources :comments
   end
 end
-
