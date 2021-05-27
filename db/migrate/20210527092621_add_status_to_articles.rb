094f3396135fbc3c7cdf1772aebe00fbf3e8ac65
Igor.Koloski <koloski_igor@hotmail.com>
2021-05-27 16:31:54 +0200

    Add linter error fixes

--- db/migrate/20210527092621_add_status_to_articles.rb
+++ db/migrate/20210527092621_add_status_to_articles.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddStatusToArticles < ActiveRecord::Migration[6.1]
   def change
     add_column :articles, :status, :string
--- db/migrate/20210527092635_add_status_to_comments.rb
+++ db/migrate/20210527092635_add_status_to_comments.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddStatusToComments < ActiveRecord::Migration[6.1]
   def change
     add_column :comments, :status, :string
