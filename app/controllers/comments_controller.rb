d4896d8d4403833922d54ea548beb245b30d14a2
Safa <safaaballagh97@gmail.com>
2021-05-27 11:42:30 +0100

    Fix destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,14 @@ class CommentsController < ApplicationController
       @comment = @article.comments.create(comment_params)
       redirect_to article_path(@article)
     end
+
+    def show
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+    end 
+  
   
     def destroy
       @article = Article.find(params[:article_id])
@@ -15,9 +23,9 @@ class CommentsController < ApplicationController
     end
   
     private
-      def comment_params
-        params.require(:comment).permit(:commenter, :body)
-      end
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
   end
   
   
